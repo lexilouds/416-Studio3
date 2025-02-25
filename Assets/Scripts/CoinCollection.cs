b365d87c2a8b96871be915fb5c419ccf1a3e32c9
lexilouds <lexiloudiadis@gmail.com>
2025-02-24 17:55:14 -0800

    Added scoring logic

--- Assets/Scripts/CoinCollection.cs
+++ Assets/Scripts/CoinCollection.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CoinCollection : MonoBehaviour
 {
     private int coin = 0;
 
+    public TextMeshProUGUI coinText;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
             coin++;
+            coinText.text = "Score: " + coin.ToString();
             Destroy(other.gameObject);
         }
     }
